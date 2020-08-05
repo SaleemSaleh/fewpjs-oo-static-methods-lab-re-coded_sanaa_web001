d9e36a8b29090badf7f9af786272d3b590254da2
Saleem Saleh <salem1234saleem@gmail.com>
2020-08-05 15:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ class Formatter {
   static sanitize  (string) {
       return string.replace(/[^A-Za-z0-9-']+/g, '');
     }
-  static titleize  () {
-        console.log('My method is static!');
+  static titleize  (str) {
+      str = str.toLowerCase().split(' ');
+      for (var i = 0; i < str.length; i++) {
+        if (str[i] ==='a' || str[i] ==='an' || str[i] ==='but' || str[i] ==='of' ||
+        str[i] ==='and' || str[i] ==='for' || str[i] ==='at' ||
+        str[i] ==='by' || str[i] ==='from') {
+          
+        }else {
+          str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+        }
+      }
+      return str.join(' ');
     }
 }
+
